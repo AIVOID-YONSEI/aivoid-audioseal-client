f74792e608edd07617d8f944d3ce4e71cb7f49c1
jinho222 <jinho020@yonsei.ac.kr>
2024-05-12 17:25:08 +0900

    loading ui

--- src/pages/landing-page.tsx
+++ src/pages/landing-page.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Container, Flex, HStack, keyframes, Image, IconButton, Text, VStack, useToast } from "@chakra-ui/react";
+import { Avatar, Box, Container, Flex, HStack, keyframes, Image, IconButton, Text, VStack, useToast, Spinner, Center } from "@chakra-ui/react";
 import { ChatIcon, CloseIcon, NotAllowedIcon, PhoneIcon } from "@chakra-ui/icons";
 import whoWhoLogo from "../assets/whowho-logo.png";
 import choonsikImg from "../assets/choonsik.jpg";
@@ -28,6 +28,7 @@ const BASE_URL = "https://axiomatic-robot-423003-b8.du.r.appspot.com";
 export default function LandingPage() {
   const toast = useToast();
   const [isRecording, setIsRecording] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const gumStream = useRef<MediaStream | null>(null);
   const recorder = useRef<MediaRecorder | null>(null);
@@ -77,6 +78,7 @@ export default function LandingPage() {
     const formData = new FormData();
     formData.append("audio", base64data.current.toString());
 
+    setIsLoading(true);
     const {
       path,
     }: {
@@ -87,6 +89,7 @@ export default function LandingPage() {
       body: formData,
     }).then((res) => res.json());
 
+    setIsLoading(false);
     await playAudio(path);
 
     // detect
@@ -168,56 +171,63 @@ export default function LandingPage() {
   };
 
   return (
-    <Box bgColor={"#eaeaea"}>
-      <Container bgColor={"white"} minW={"330px"} px={0}>
-        <VStack justifyContent={"space-between"} h={"100vh"} alignItems={"stretch"} spacing={0}>
-          <HStack py={4} px={4} justifyContent={"center"}>
-            <Image src={whoWhoLogo} w={"150px"} />
-          </HStack>
-          <Flex justifyContent="center" alignItems="center" h="250px" w="full" overflow="hidden" bgColor={"#eaeaea"} flexGrow={1}>
-            <Box
-              as="div"
-              position="relative"
-              w={"96px"}
-              h={"96px"}
-              _before={{
-                content: "''",
-                position: "relative",
-                display: "block",
-                width: "300%",
-                height: "300%",
-                boxSizing: "border-box",
-                marginLeft: "-100%",
-                marginTop: "-100%",
-                borderRadius: "50%",
-                bgColor: "teal",
-                animation: `2.25s ${pulseRing} cubic-bezier(0.455, 0.03, 0.515, 0.955) -0.4s infinite`,
-              }}
-            >
-              <Avatar src={choonsikImg} size="full" position="absolute" top={0} />
-            </Box>
-          </Flex>
-          <VStack py={4} borderBottomWidth={"1px"} borderBottomColor={"gray.200"} borderBottomStyle={"solid"}>
-            <Text fontWeight={"bold"} fontSize={"18px"}>
-              저장되지 않은 번호
-            </Text>
-            <Text fontSize={"14px"} color={"gray"}>
-              010-1234-5678
-            </Text>
-          </VStack>
-          <VStack py={4} minH={"100px"} spacing={8}>
-            <HStack justifyContent={"center"} spacing={15}>
-              <IconButton icon={isRecording ? <NotAllowedIcon /> : <PhoneIcon />} aria-label="call" variant={"ghost"} onClick={onClickCallButton} />
-              <IconButton icon={<ChatIcon />} aria-label="chat" variant={"ghost"} onClick={onClickChatButton} />
-              <input type="file" accept="audio/*" hidden ref={fileInput} onChange={onFileChange} />
-              <IconButton icon={<CloseIcon />} aria-label="close" variant={"ghost"} colorScheme="red" />
+    <>
+      <Box bgColor={"#eaeaea"}>
+        <Container bgColor={"white"} minW={"330px"} px={0}>
+          <VStack justifyContent={"space-between"} h={"100vh"} alignItems={"stretch"} spacing={0}>
+            <HStack py={4} px={4} justifyContent={"center"}>
+              <Image src={whoWhoLogo} w={"150px"} />
             </HStack>
-            <Text color={"#666"} fontSize={"small"} fontWeight={"semi-bold"}>
-              @Copyright 2024년도 1학기 ICT경영 1조
-            </Text>
+            <Flex justifyContent="center" alignItems="center" h="250px" w="full" overflow="hidden" bgColor={"#eaeaea"} flexGrow={1}>
+              <Box
+                as="div"
+                position="relative"
+                w={"96px"}
+                h={"96px"}
+                _before={{
+                  content: "''",
+                  position: "relative",
+                  display: "block",
+                  width: "300%",
+                  height: "300%",
+                  boxSizing: "border-box",
+                  marginLeft: "-100%",
+                  marginTop: "-100%",
+                  borderRadius: "50%",
+                  bgColor: "teal",
+                  animation: `2.25s ${pulseRing} cubic-bezier(0.455, 0.03, 0.515, 0.955) -0.4s infinite`,
+                }}
+              >
+                <Avatar src={choonsikImg} size="full" position="absolute" top={0} />
+              </Box>
+            </Flex>
+            <VStack py={4} borderBottomWidth={"1px"} borderBottomColor={"gray.200"} borderBottomStyle={"solid"}>
+              <Text fontWeight={"bold"} fontSize={"18px"}>
+                저장되지 않은 번호
+              </Text>
+              <Text fontSize={"14px"} color={"gray"}>
+                010-1234-5678
+              </Text>
+            </VStack>
+            <VStack py={4} minH={"100px"} spacing={8}>
+              <HStack justifyContent={"center"} spacing={15}>
+                <IconButton icon={isRecording ? <NotAllowedIcon /> : <PhoneIcon />} aria-label="call" variant={"ghost"} onClick={onClickCallButton} />
+                <IconButton icon={<ChatIcon />} aria-label="chat" variant={"ghost"} onClick={onClickChatButton} />
+                <input type="file" accept="audio/*" hidden ref={fileInput} onChange={onFileChange} />
+                <IconButton icon={<CloseIcon />} aria-label="close" variant={"ghost"} colorScheme="red" />
+              </HStack>
+              <Text color={"#666"} fontSize={"small"} fontWeight={"semi-bold"}>
+                @Copyright 2024년도 1학기 ICT경영 1조
+              </Text>
+            </VStack>
           </VStack>
-        </VStack>
-      </Container>
-    </Box>
+        </Container>
+      </Box>
+      {isLoading && (
+        <Center pos={"fixed"} top={0} left={0} right={0} bottom={0} backgroundColor={"gray"} opacity={"0.5"}>
+          <Spinner thickness="5px" speed="0.65s" emptyColor="gray.200" color="teal" size="xl" opacity={1} />
+        </Center>
+      )}
+    </>
   );
 }
